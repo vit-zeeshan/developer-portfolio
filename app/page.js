a813cebe6dfc6e2c87a01f03277efceaa68a6ef4
zeeshan <zeeshan.vit@gmail.com>
2025-04-07 17:26:56 +0530

    fix: disable SSR for GlowCard to fix document reference error

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
