3f47d5935e20eaae26c0621f7c558526dc592181
zeeshan <zeeshan.vit@gmail.com>
2025-04-07 16:29:11 +0530

    Removed blog section and related components

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
           </li>
           <li>
-            {/* <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link> */}
+            {/* <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href=""><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link> */}
           </li>
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
