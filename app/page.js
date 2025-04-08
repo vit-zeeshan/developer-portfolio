bfbb10e976617559cb82eaa520f5832cb420a118
zeeshan <zeeshan.vit@gmail.com>
2025-04-08 11:48:12 +0530

    updated pagejs

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+import dynamic from "next/dynamic";
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
 
 export default function Home() {
   return (
