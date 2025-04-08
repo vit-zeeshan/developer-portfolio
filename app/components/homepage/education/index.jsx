e97bb5f76618fb00ec65fd579a9a44c0112fabb5
zeeshan <zeeshan.vit@gmail.com>
2025-04-08 11:32:17 +0530

    fix: dynamic import GlowCard in experience component

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import lottieFile from '../../../assets/lottie/study.json';
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
-// import dynamic from 'next/dynamic';
-// const GlowCard = dynamic(() => import('../../helper/glow-card'), { ssr: false });
+import dynamic from 'next/dynamic';
+
+const GlowCard = dynamic(() => import('../../helper/glow-card'), { ssr: false });
 
 
 function Education() {
